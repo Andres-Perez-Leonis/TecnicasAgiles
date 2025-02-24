187093027fff154d0ff685bc08a1b815342c31e1
Andres Perez Leonis <apleonis@gmail.com>
2025-02-24 17:45:42 +0100

    Funcionalidad para deteccion de palindromos añadido

--- src/main/java/demoapp/controller/PalindromeData.java
+++ src/main/java/demoapp/controller/PalindromeData.java
@@ -1,4 +1,16 @@
 package demoapp.controller;
 
+import javax.validation.constraints.Size;
+
 public class PalindromeData {
+    @Size(min=3, max=30)
+    String palabra;
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
 }
