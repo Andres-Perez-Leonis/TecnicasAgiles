90451935dae6552b91b5dedc536baf6412563617
Andres Perez Leonis <apleonis@gmail.com>
2025-02-24 17:44:42 +0100

    Servicio para Palindromos añadido

--- src/main/java/demoapp/service/PalindromoService.java
+++ src/main/java/demoapp/service/PalindromoService.java
@@ -1,4 +1,11 @@
 package demoapp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PalindromoService {
+    public boolean isPalindrome(String palabra) {
+        String invertida = new StringBuilder(palabra).reverse().toString();
+        return palabra.equalsIgnoreCase(invertida);
+    }
 }
